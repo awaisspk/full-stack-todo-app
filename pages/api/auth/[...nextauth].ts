89553e47f07288ca6f5bf8c9492beab10bf72701
awaisspk <iamawaisiqball@gmail.com>
2022-01-14 02:21:07 +0500

    feat(auth): add nextauth api route

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,12 @@
 import NextAuth from 'next-auth';
+import {PrismaAdapter} from '@next-auth/prisma-adapter';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-// import EmailProvider from "next-auth/providers/email"
+import {prisma} from '@lib/prisma';
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
-    // EmailProvider({
-    //   server: process.env.EMAIL_SERVER,
-    //   from: process.env.EMAIL_FROM,
-    // }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
@@ -20,12 +18,11 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
+
   secret: process.env.SECRET,
 
   session: {
     strategy: 'jwt',
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-    updateAge: 24 * 60 * 60, // 24 hours
   },
 
   jwt: {
@@ -33,19 +30,10 @@ export default NextAuth({
   },
 
   pages: {
-    // signIn: '/auth/signin',  // Displays signin buttons
-    // signOut: '/auth/signout', // Displays form with sign out button
-    // error: '/auth/error', // Error code passed in query string as ?error=
-    // verifyRequest: '/auth/verify-request', // Used for check email page
-    // newUser: null // If set, new users will be directed here on first sign in
+    signIn: '/auth/signin',
   },
 
-  callbacks: {
-    // async signIn({ user, account, profile, email, credentials }) { return true },
-    // async redirect({ url, baseUrl }) { return baseUrl },
-    // async session({ session, token, user }) { return session },
-    // async jwt({ token, user, account, profile, isNewUser }) { return token }
-  },
+  callbacks: {},
 
   events: {},
   debug: false,
