e9b6972db90399c4fa0f5c812b7659e4139302ee
awaisspk <iamawaisiqball@gmail.com>
2022-01-14 02:22:50 +0500

    feat(react-query): add Queryclient provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import {globalStyles} from '@globalCss';
 import type {AppProps} from 'next/app';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {ReactQueryDevtools} from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 function MyApp({Component, pageProps}: AppProps) {
   globalStyles();
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
