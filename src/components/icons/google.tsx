978e8841a920c886d3f25d133ef1abbd406af56f
awaisspk <iamawaisiqball@gmail.com>
2022-01-14 02:26:11 +0500

    feat(component): add github and google icon

---
+++ src/components/icons/google.tsx
@@ -0,0 +1,20 @@
+import {SVGProps} from 'react';
+
+export const GoogleIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-brand-google"
+    width={25}
+    height={25}
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="rgba(255,255,255,0.8)"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M0 0h24v24H0z" stroke="none" />
+    <path d="M17.788 5.108A9 9 0 1 0 21 12h-8" />
+  </svg>
+);
