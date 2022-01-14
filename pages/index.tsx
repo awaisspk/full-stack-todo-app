3d3d892c0fd9d5d20d775d8aea9989128b588c1c
awaisspk <iamawaisiqball@gmail.com>
2022-01-14 13:50:07 +0500

    refactor(page): move markup to header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,13 @@
 import type {NextPage} from 'next';
-import {useSession} from '@hooks/useSession';
-import {signOut} from 'next-auth/react';
+import {HomePageLayout} from '@src/Layouts/HomepageLayout';
+import {Header} from '@components/Header';
 
 const Home: NextPage = () => {
-  const [session] = useSession({
-    required: true,
-    redirectTo: '/auth/signin',
-    queryConfig: {
-      staleTime: 60 * 1000 * 60 * 3, // 3 hours
-      refetchInterval: 60 * 1000 * 5, // 5 minutes
-    },
-  });
-
-  if (session) {
-    const {user} = session;
-
-    return (
-      <div>
-        <span>Home page</span>
-        <button
-          onClick={() =>
-            signOut({
-              callbackUrl: `${window.location.origin}/auth/signin`,
-            })
-          }
-        >
-          signOut
-        </button>
-        <div>{user?.email}</div>
-        {user?.image && (
-          <div
-            style={{
-              borderRadius: '9999px',
-              overflow: 'hidden',
-              position: 'relative',
-              display: 'block',
-            }}
-          >
-            {/* <img src={user.image} alt="Profile image" /> */}
-          </div>
-        )}
-      </div>
-    );
-  }
-
-  if (!session) {
-    return <div>not session found</div>;
-  }
-
-  return <div>user not found</div>;
+  return (
+    <HomePageLayout>
+      <Header />
+    </HomePageLayout>
+  );
 };
 
 export default Home;
