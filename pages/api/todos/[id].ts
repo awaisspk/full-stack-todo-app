40006b439d95cb5f951bbeaee72203ef6f70ed3d
awaisspk <iamawaisiqball@gmail.com>
2022-01-19 14:00:36 +0500

    feat(api): endpoint to delete todo

--- pages/api/todos/[id].ts
+++ pages/api/todos/[id].ts
@@ -8,30 +8,52 @@ type Data =
       id: String;
       content: String;
       completed: Boolean;
-    }
+    }[]
   | {error: string};
+
+type ReqType = {
+  completed?: boolean;
+  delete?: boolean;
+};
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const session = await getSession({req});
   const token = await getToken({req, secret: process.env.SECRET});
 
   const {id} = req.query;
-  const {completed} = req.body;
+
+  const todo: ReqType = req.body;
 
   if (session) {
     try {
-      const todo = await prisma.todo.update({
-        data: {
-          completed,
+      if (todo.completed) {
+        await prisma.todo.update({
+          data: {
+            completed: todo.completed,
+            userId: token?.sub,
+          },
+          where: {
+            // @ts-ignore
+            id: id,
+          },
+        });
+      } else if (todo.delete) {
+        await prisma.todo.delete({
+          where: {
+            // @ts-ignore
+            id,
+          },
+        });
+      }
+
+      const todos = await prisma.todo.findMany({
+        where: {
           userId: token?.sub,
         },
-        where: {
-          // @ts-ignore
-          id: id,
-        },
       });
+
       res.status(200);
-      return res.json(todo);
+      res.json(todos);
     } catch (error) {
       res.status(401);
       res.json({error: 'User already exists'});
