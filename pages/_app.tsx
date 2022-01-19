106f06d1828167d7747fe2a093544ab0f49c52e1
awaisspk <iamawaisiqball@gmail.com>
2022-01-19 12:28:39 +0500

    refactor: remove react query dev tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import {globalStyles} from '@globalCss';
 import type {AppProps} from 'next/app';
 import {QueryClient, QueryClientProvider} from 'react-query';
-import {ReactQueryDevtools} from 'react-query/devtools';
 
 const queryClient = new QueryClient();
 
@@ -9,7 +8,6 @@ function MyApp({Component, pageProps}: AppProps) {
   globalStyles();
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
