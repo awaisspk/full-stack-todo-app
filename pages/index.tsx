cf51d22aae4a02a79667cac82d57097a096d3afb
awaisspk <iamawaisiqball@gmail.com>
2022-01-19 12:29:57 +0500

    feat(page): fetch and display all todos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,53 @@
-import type {NextPage} from 'next';
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+  NextPage,
+} from 'next';
 import {HomePageLayout} from '@src/Layouts/HomepageLayout';
 import {Header} from '@components/Header';
+import {TodoList} from '@components/Todo';
+import {prisma} from '@lib/prisma';
+import {getSession} from 'next-auth/react';
 
-const Home: NextPage = () => {
+const Home: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({todos}) => {
+  console.log(todos);
   return (
     <HomePageLayout>
       <Header />
+      <TodoList todoslist={todos} />
     </HomePageLayout>
   );
+};
+
+export const getServerSideProps = async ({req}: GetServerSidePropsContext) => {
+  const session = await getSession({req});
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth/signin',
+        permanent: false,
+      },
+    };
+  }
+
+  if (session) {
+    const todos = await prisma.todo.findMany({
+      where: {
+        user: {
+          email: session.user?.email,
+        },
+      },
+    });
+
+    return {
+      props: {
+        todos,
+      },
+    };
+  }
 };
 
 export default Home;
